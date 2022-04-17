d665428650141611df6ccb8e814f3861d7a446c2
fadihere <fadihere@outlook.com>
2022-04-18 01:14:16 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playsound(int note) {
-    final player = AudioCache();
-    player.play('note$note.wav');
-  }
+  const XylophoneApp({Key? key}) : super(key: key);
 
-  Expanded Keys({color, soundnumber}) {
+  Expanded keys({color, soundnumber}) {
     return Expanded(
-      child: MaterialButton(
-        onPressed: () {
-          playsound(soundnumber);
-        },
-        color: color,
-      ),
-    );
+        child: MaterialButton(
+            onPressed: () => _playsound(note: soundnumber), color: color));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Keys(color: Colors.red, soundnumber: 1),
-              Keys(color: Colors.orange, soundnumber: 2),
-              Keys(color: Colors.yellow, soundnumber: 3),
-              Keys(color: Colors.green, soundnumber: 4),
-              Keys(color: Colors.blue, soundnumber: 5),
-              Keys(color: Colors.indigo, soundnumber: 6),
-              Keys(color: Colors.purple, soundnumber: 7),
+            children: [
+              keys(color: Colors.red, soundnumber: 1),
+              keys(color: Colors.orange, soundnumber: 2),
+              keys(color: Colors.yellow, soundnumber: 3),
+              keys(color: Colors.green, soundnumber: 4),
+              keys(color: Colors.blue, soundnumber: 5),
+              keys(color: Colors.indigo, soundnumber: 6),
+              keys(color: Colors.purple, soundnumber: 7),
             ],
           ),
         ),
       ),
-      debugShowCheckedModeBanner: false,
     );
+  }
+
+  _playsound({required int note}) {
+    final player = AudioCache();
+    player.play("note$note.wav");
   }
 }
